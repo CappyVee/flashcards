8a31150d47f95a616a42b10ebf46ca94fb90271d
Chris Quattrocchi and Zachary Marston <zachary@>
2017-10-17 11:57:59 -0700

    create initial cards without description.

---
+++ js/scripts.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+  $(".js").click(function() {
+    $(".jsfc").toggle();
+  });
+  $(".op").click(function() {
+    $(".opfc").toggle();
+  });
+  $(".var").click(function() {
+    $(".varfc").toggle();
+  });
+  $(".vn").click(function() {
+    $(".vnfc").toggle();
+  });
+  $(".fun").click(function() {
+    $(".funfc").toggle();
+  });
+  $(".met").click(function() {
+    $(".metfc").toggle();
+  });
+  $(".arg").click(function() {
+    $(".argfc").toggle();
+  });
+  $(".par").click(function() {
+    $(".parfc").toggle();
+  });
+  $(".ret").click(function() {
+    $(".retfc").toggle();
+  });
+  $(".cm").click(function() {
+    $(".cmfc").toggle();
+  });
+  $(".str").click(function() {
+    $(".strfc").toggle();
+  });
+  $(".boo").click(function() {
+    $(".boofc").toggle();
+  });
+  $(".un").click(function() {
+    $(".unfc").toggle();
+  });
+  $(".nan").click(function() {
+    $(".nanfc").toggle();
+  });
+  $(".esc").click(function() {
+    $(".escfc").toggle();
+  });
+  $(".alt").click(function() {
+    $(".altfc").toggle();
+  });
+  $(".com").click(function() {
+    $(".comfc").toggle();
+  });
+  $(".jqu").click(function() {
+    $(".jqufc").toggle();
+  });
+  $(".att").click(function() {
+    $(".attfc").toggle();
+  });
+
+});
